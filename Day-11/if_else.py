1ef4f239f32423aeb3f6a50b44bb4f4d185999ee
Srinivas Kandagatla <srinivas.kandagatla112004@gmail.com>
2026-09-05 15:15:29 +0530

    Conditional Statements (if and if-else)

---
+++ Day-11/if_else.py
@@ -0,0 +1,85 @@
+"""user_name=input()
+password=input()
+if user_name=="admin" and password=="admin123":
+    print("Login Successfull")
+else:
+    print("Invalid Crenditials")
+
+products=["watch","Laptop","Mobile"]
+search=input()
+if search in products:
+    print("Product Found")
+else:
+    print("Proudct Not Found")"""
+
+"""price=int(input())
+if price>99:
+    print(f"Final Bill Amount : {price}")
+else:
+    print(f"Final Bill Amount : {price+30}")"""
+
+"""Budget=int(input())
+if Budget>10000:
+    print("Trip")
+elif Budget>5000:
+    print("Resort")
+elif Budget>3000:
+    print("Movie and Dinner")
+elif Budget>1000:
+    print("Cafe and Shopping")
+elif Budget>500:
+    print("Street Food and Park Visit")
+else:
+    print("Stay Home")"""
+    
+"""Hour=int(input("Enter the Time : "))
+if 5<=Hour<=11:
+    print("Good Morning")
+elif 12<=Hour<=16:
+    print("Good Afternoon")
+elif 17<=Hour<=20:
+    print("Good Evening")
+else:
+    print("Good Night")
+
+budget=int(input())
+if budget>10000:
+    print("Colud Hosting")
+elif budget>5000:
+    print("Business Hosting")
+elif budget>2000:
+    print("Premium Hosting")
+else:
+    print("Single Hosting")
+
+f=eval(input())
+c=eval(input())
+if f:
+    if c:
+        print("Story Visible")
+    else:
+        print("Not in Close Friends List")
+else:
+    print("Follow the Account First")
+
+r=eval(input("Resgistere : "))
+fe=eval(input("Fee Paid : "))
+if r:
+    if fe:
+        print("Tournament Entry Confirmed")
+    else:
+        print("Entry Fee Pending")
+else:
+    print("Registration Required")"""
+
+l=eval(input("Link Active: "))
+p=eval(input("Permission Granted : "))
+if l:
+    if p:
+        print("File Opened Successfully")
+    else:
+        print("Access Denied")
+else:
+    print("Invalid File Link")
+
+
---
+++ Day-11/simple_if.py
@@ -0,0 +1,12 @@
+"""sales=int(input())
+if sales>1000:
+    print("Best Seller")
+
+e_s=bool(input("Eligible Account :"))
+ver_sub=bool(input("Meta verified Subscription :"))
+if e_s and ver_sub:
+    print("Verfied Badge Granted")"""
+
+status=eval(input())
+if status:
+    print("Its Raning")
